2d4b47a724f9bf0b6c5da32a761c3678422c9aeb
batool2azzam <122049115+batool2azzam@users.noreply.github.com>
2023-09-02 10:12:13 +0300

    firt commit

---
+++ react-Ass1/src/component/Post.js
@@ -0,0 +1,12 @@
+
+function Post (){
+
+    return <div className="post">
+    <h1>this is post title</h1>       
+    <br/>
+    <hr/>
+
+    <h3>this is post body</h3>
+    </div>
+}
+export   {Post};
